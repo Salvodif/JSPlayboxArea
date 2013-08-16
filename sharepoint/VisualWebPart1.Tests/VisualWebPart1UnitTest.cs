630a78d57f62b50222de91b02b59f6116656e201
Salvodif <salvatore.difazio@gmail.com>
2013-08-16 12:17:38 +0200

    first commit

---
+++ sharepoint/VisualWebPart1.Tests/VisualWebPart1UnitTest.cs
@@ -0,0 +1,21 @@
+﻿namespace VisualWebPart1.Tests
+{
+	using System;
+	using Microsoft.QualityTools.Testing.Emulators;
+	using Microsoft.SharePoint.Emulators;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[ TestClass ]
+	public class VisualWebPart1UnitTest
+	{
+		[ TestMethod ]
+		public void TestMethod1 ( )
+		{
+			using ( new SharePointEmulationScope ( EmulationMode.Enabled ) )
+			{
+				//var vwp = new VisualWebPart1 ( );
+
+			}
+		}
+	}
+}
